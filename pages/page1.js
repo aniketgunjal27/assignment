c7e780212f2ddfa9664ac5fcf6f782a6c2eb78c2
Aniket Gunjal <aniket.gunjal@bluepineapple.io>
2022-04-20 16:12:14 +0530

    reusable component

--- pages/page1.js
+++ pages/page1.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import DATA from '../components/Sampledata'
 const toyota = (props) => {
   return (
     <div>
@@ -11,29 +12,31 @@ const toyota = (props) => {
             <div>
               <div className="max-w-md rounded overflow-hidden shadow-lg">
                 <img
-                  className="w-full"
+                  className="object-contain h-255 w-455"
                   src={car.photos[0].url}
                   alt="Cards for drive.au"
                 />
                 <div className="px-6 py-4">
-                  <div className="font-bold text-xl mb-2">
+                  <div className="font-sans font-medium text-xl mb-2">
                    {car.redbook.title}
+
                   </div>
-                  <p className="text-gray-700 text-base">
-                    <div className="grid grid-cols-2 gap-4">
-                      <div className="list-disc"> { ` ${car.redbook.doors}Doors,  ${car.redbook.seats}Seats` } </div>
-                      <div className="list-disc">{car.redbook.engine}</div>
-                      <div className="list-disc">{ ` ${car.redbook.gears} Speed  ${car.gearType}` }</div>
+                  <p className="text-gray-700 text-base font-sans font-medium divide-y ">
+                    <div className="grid grid-cols-2 gap-4 ">
+                      <div className="list-disc" >  { ` ${car.redbook.doors } Doors,  ${car.redbook.seats} Seats` } </div>
+                      <div>{car.redbook.engine}</div>
+                      <div>{ ` ${car.redbook.gears } Speed  ${car.gearType}` }</div>
                       
-                      <div  className="list-disc"> {car.driveCode}</div>
+                      <div> {car.driveCode}</div>
                      
                       <div> 
                         
                       </div>
                     </div>
-                    <div className="grid grid-cols-2 gap-4">
+                   
+                    <div className=" grid grid-cols-2 gap-y-2 pt-3 ">
                       <div>Average Kms</div>
-                      <div className="font-bold">{car.redbook.pricing.avgKmsMin}k-{car.redbook.pricing.avgKmsMax}k</div>
+                      <div className="font-bold  ">{car.redbook.pricing.avgKmsMin}k-{car.redbook.pricing.avgKmsMax}k</div>
                       <div>Trade in Value</div>
                       <div className="font-bold">${car.redbook.pricing.tradeMin}-${car.redbook.pricing.tradeMax}</div>
                       <div>Private Sale</div>
@@ -42,8 +45,8 @@ const toyota = (props) => {
                   </p>
                 </div>
 
-                <li className="flex justify-center py-2 px-2">
-                  <button className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-32 rounded  ">
+                <li className="font-sans flex justify-center py-4 px-2">
+                  <button className=" bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-32 rounded-lg  ">
                     Get Offer Now
                   </button>
                 </li>
@@ -59,83 +62,83 @@ const toyota = (props) => {
 };
 
 export async function getStaticProps() {
-  var data = {
-    cardData: [
-      {
-        variantId: 1,
-        vehicleKey: "AUVABAR2011AEAA",
-        segmentId: 1,
-        gearType: "Manual",
-        driveCode: "FWD",
-        photos: [
-          {
-            url: "https://res.cloudinary.com/driveau/image/upload/v1616375245/vehicles/redbook/AUVABAR2011AEAA/ABAR0016.jpg",
-            publicId: "vehicles/redbook/AUVABAR2011AEAA/ABAR0016",
-          },
-          {
-            url: "https://res.cloudinary.com/driveau/image/upload/v1616375245/vehicles/redbook/AUVABAR2011AEAA/ABAR0025.jpg",
-            publicId: "vehicles/redbook/AUVABAR2011AEAA/ABAR0025",
-          },
-        ],
-        redbook: {
-          title: "Series 1 Esseesse Hatchback 3dr Man 5sp 1.4T [Mar]",
-          doors: 3,
-          seats: 4,
-          engine: "1.4",
-          gears: 5,
-          model: null,
-          make: "ABAR",
-          year: 2011,
-          vehicleType: "PS",
-          pricing: {
-            avgRetail: 9750,
-            tradeMax: 8000,
-            tradeMin: 6200,
-            privateMin: 8700,
-            privateMax: 10500,
-            avgKmsMax: 180,
-            avgKmsMin: 100,
-          },
-        },
-      },
-      {
-        variantId: 2,
-        vehicleKey: "AUVABAR2011AEAC",
-        segmentId: 1,
-        gearType: "Auto",
-        driveCode: "FWD",
-        photos: [
-          {
-            url: "https://res.cloudinary.com/driveau/image/upload/v1616375245/vehicles/redbook/AUVABAR2011AEAC/ABAR0012.jpg",
-            publicId: "vehicles/redbook/AUVABAR2011AEAC/ABAR0012",
-          },
-        ],
-        redbook: {
-          title: "Series 1 Tributo Ferrari Hatchback 3dr MTA 5sp 1.4T [May]",
-          doors: 3,
-          seats: 4,
-          engine: "1.4",
-          gears: 5,
-          model: null,
-          make: "ABAR",
-          year: 2011,
-          vehicleType: "PS",
-          pricing: {
-            avgRetail: 22900,
-            tradeMax: 19600,
-            tradeMin: 16600,
-            privateMin: 20900,
-            privateMax: 23900,
-            avgKmsMax: 180,
-            avgKmsMin: 100,
-          },
-        },
-      },
-    ],
-  };
+  // var data = {
+  //   cardData: [
+  //     {
+  //       variantId: 1,
+  //       vehicleKey: "AUVABAR2011AEAA",
+  //       segmentId: 1,
+  //       gearType: "Manual",
+  //       driveCode: "FWD",
+  //       photos: [
+  //         {
+  //           url: "https://res.cloudinary.com/driveau/image/upload/v1616375245/vehicles/redbook/AUVABAR2011AEAA/ABAR0016.jpg",
+  //           publicId: "vehicles/redbook/AUVABAR2011AEAA/ABAR0016",
+  //         },
+  //         {
+  //           url: "https://res.cloudinary.com/driveau/image/upload/v1616375245/vehicles/redbook/AUVABAR2011AEAA/ABAR0025.jpg",
+  //           publicId: "vehicles/redbook/AUVABAR2011AEAA/ABAR0025",
+  //         },
+  //       ],
+  //       redbook: {
+  //         title: "Series 1 Esseesse Hatchback 3dr Man 5sp 1.4T [Mar]",
+  //         doors: 3,
+  //         seats: 4,
+  //         engine: "1.4",
+  //         gears: 5,
+  //         model: null,
+  //         make: "ABAR",
+  //         year: 2011,
+  //         vehicleType: "PS",
+  //         pricing: {
+  //           avgRetail: 9750,
+  //           tradeMax: 8000,
+  //           tradeMin: 6200,
+  //           privateMin: 8700,
+  //           privateMax: 10500,
+  //           avgKmsMax: 180,
+  //           avgKmsMin: 100,
+  //         },
+  //       },
+  //     },
+  //     {
+  //       variantId: 2,
+  //       vehicleKey: "AUVABAR2011AEAC",
+  //       segmentId: 1,
+  //       gearType: "Auto",
+  //       driveCode: "FWD",
+  //       photos: [
+  //         {
+  //           url: "https://res.cloudinary.com/driveau/image/upload/v1616375245/vehicles/redbook/AUVABAR2011AEAC/ABAR0012.jpg",
+  //           publicId: "vehicles/redbook/AUVABAR2011AEAC/ABAR0012",
+  //         },
+  //       ],
+  //       redbook: {
+  //         title: "Series 1 Tributo Ferrari Hatchback 3dr MTA 5sp 1.4T [May]",
+  //         doors: 3,
+  //         seats: 4,
+  //         engine: "1.4",
+  //         gears: 5,
+  //         model: null,
+  //         make: "ABAR",
+  //         year: 2011,
+  //         vehicleType: "PS",
+  //         pricing: {
+  //           avgRetail: 22900,
+  //           tradeMax: 19600,
+  //           tradeMin: 16600,
+  //           privateMin: 20900,
+  //           privateMax: 23900,
+  //           avgKmsMax: 180,
+  //           avgKmsMin: 100,
+  //         },
+  //       },
+  //     },
+  //   ],
+  // };
 
   return {
-    props: data,
+    props: DATA,
   };
 }
 
